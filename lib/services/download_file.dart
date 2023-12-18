d68c50a42b4899e4d7234074c38a548a81b5a692
IyadAlanssary <77411974+IyadAlanssary@users.noreply.github.com>
2023-12-18 23:15:56 +0300

    Clean Up

--- lib/services/download_file.dart
+++ lib/services/download_file.dart
@@ -1,54 +1,32 @@
 import "package:network_applications/constants/api.dart";
 import 'dart:html' as html;
+import 'package:shared_preferences/shared_preferences.dart';
 
-void downloadFile(int fileId) {
+Future<void> downloadFile(int fileId) async {
   String url = "$localHostApi/files/$fileId/download";
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  var cache = pref.getString("token");
 
   html.HttpRequest request = html.HttpRequest();
   request.open('GET', url);
-  request.setRequestHeader('Authorization', 'Bearer $token');
-
-  // String fileName = header.split(';')[1].split('=')[1].replaceAll('"', '');
-
+  request.setRequestHeader('Authorization', 'Bearer $cache');
   request.responseType = 'blob';
 
   request.onLoad.listen((event) {
+
+    var contentDisposition = request.responseHeaders['content-disposition'];
+    print('Content-Disposition: $contentDisposition');
+    String? header = request.getResponseHeader('Content-Disposition');
+    print('Content-Disposition: $header');
+
+    String? fileName = header?.split(';')[1].split('=')[1].replaceAll('"', '');
+
     html.AnchorElement anchorElement = html.AnchorElement(
         href: html.Url.createObjectUrlFromBlob(request.response));
-    String? header = request.getResponseHeader('Content-Disposition');
     print(request.status);
-    print(header);
 
-    anchorElement.download = header;
+    anchorElement.download = fileName;
     anchorElement.click();
   });
   request.send();
 }
-
-// request.response.headers.set('Access-Control-Expose-Headers', 'Content-Disposition');
-// request.response.headers.set('Content-Disposition', 'attachment; filename="test.txt"');
-
-
-//
-// void downloadFile2(int folderId) {
-//   String url = "$localHostApi/files/download/$folderId";
-//
-//   http.get(Uri.parse(url), headers: {'Authorization': 'Bearer $token'}).then((response) {
-//     // Get the file name from the header
-//     String header = response.headers['content-disposition']!;
-//     // Extract the file name from the header
-//     String fileName = header.split(';')[1].split('=')[1].replaceAll('"', '');
-//     // Create a temporary file with the file name
-//     List<int> bytes = response.bodyBytes;
-//     // Map<String, String> options = {'type': fileName};
-//     html.File tempFile = html.File(bytes, fileName);
-//     // Write the file to the local file system
-//     html.FileSaver saver = html.FileSaver();
-//     saver.saveAs(tempFile, fileName);
-//     // // Write the response body to the file
-//     // tempFile.writeAsBytes(response.bodyBytes).then((file) {
-//       // Open the file with the default application
-//       Process.run('open', [file.path]);
-//     });
-//   });
-// }
