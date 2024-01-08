faabe55c6b128ef048a6e1b4a6b54d7c59081b1f
IyadAlanssary <77411974+IyadAlanssary@users.noreply.github.com>
2024-01-08 03:14:46 +0300

    Update style and colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-const primary = Color(0xFF33CCCC);
+const primary = Color(0xFF3a0402);
 
 const lightColorScheme = ColorScheme(
   brightness: Brightness.light,
-  primary: Color(0xFF33CCCC),
+  primary: Color(0xffbd4700),
   onPrimary: Color(0xFFFFFFFF),
   primaryContainer: Color(0xFFFFDAD5),
   onPrimaryContainer: Color(0xFF410001),
@@ -21,10 +21,10 @@ const lightColorScheme = ColorScheme(
   errorContainer: Color(0xFFFFDAD6),
   onErrorContainer: Color(0xFF410002),
   outline: Color(0xFFDDDDDD),
-  background: Color(0xFFFFFFFF),
-  onBackground: Color(0xFF000000),
+  background: Color(0xffffe4b3),
+  onBackground: Color(0xFF3a0402),
   surface: Color(0xFFFFFFFF),
-  onSurface: Color(0xFF000000),
+  onSurface: Color(0xFF3a0402),
   surfaceVariant: Color(0xFFF3F4F6),
   onSurfaceVariant: Color(0xFF52443B),
   inverseSurface: Color(0xFF362F2B),
@@ -34,7 +34,41 @@ const lightColorScheme = ColorScheme(
   surfaceTint: Color(0xFF934B00),
   outlineVariant: Color(0xFFD6C3B7),
   scrim: Color(0xFF000000),
+
 );
+// const lightColorScheme = ColorScheme(
+//   brightness: Brightness.light,
+//   primary: Color(0xFF33CCCC),
+//   onPrimary: Color(0xFFFFFFFF),
+//   primaryContainer: Color(0xFFFFDAD5),
+//   onPrimaryContainer: Color(0xFF410001),
+//   secondary: Color(0xFFF9881F),
+//   onSecondary: Color(0xFFFFFFFF),
+//   secondaryContainer: Color(0xFFDAFAE5),
+//   onSecondaryContainer: Color(0xFF127835),
+//   tertiary: Color(0xFF5E6135),
+//   onTertiary: Color(0xFFFFFFFF),
+//   tertiaryContainer: Color(0xFFE4E6AF),
+//   onTertiaryContainer: Color(0xFF1B1D00),
+//   error: Color(0xFFBA1A1A),
+//   onError: Color(0xFFFFFFFF),
+//   errorContainer: Color(0xFFFFDAD6),
+//   onErrorContainer: Color(0xFF410002),
+//   outline: Color(0xFFDDDDDD),
+//   background: Color(0xFFFFFFFF),
+//   onBackground: Color(0xFF000000),
+//   surface: Color(0xFFFFFFFF),
+//   onSurface: Color(0xFF000000),
+//   surfaceVariant: Color(0xFFF3F4F6),
+//   onSurfaceVariant: Color(0xFF52443B),
+//   inverseSurface: Color(0xFF362F2B),
+//   onInverseSurface: Color(0xFFFBEEE8),
+//   inversePrimary: Color(0xFFFFB782),
+//   shadow: Color(0x33000000),
+//   surfaceTint: Color(0xFF934B00),
+//   outlineVariant: Color(0xFFD6C3B7),
+//   scrim: Color(0xFF000000),
+// );
 
 const darkColorScheme = ColorScheme(
   brightness: Brightness.dark,
